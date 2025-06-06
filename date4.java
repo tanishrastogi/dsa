6e38eba3d4c5ea2fa78ce6c014311c598ce94d36
charlie0054a <charliefernandis28@gmail.com>
2025-06-06 12:26:35 +0530

    day4 completed

--- date4.java
+++ date4.java
@@ -1,21 +1,26 @@
+
 import java.util.*;
 
 public class date4 {
+
     public static void main(String[] args) {
-        
+
         System.out.println("Enter Number of elements in the array.");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int arr[] = new int[n];
 
-        for(int i=0;i<n;i++){
-            System.out.print("Element "+i+": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Element " + i + ": ");
             arr[i] = sc.nextInt();
         }
 
         // int[] a= problem238(arr);
-        problem560(arr,5);
+        // problem560(arr,0);
+        System.out.println("Enter the value of k: ");
+        int k = sc.nextInt();
+        problem974(arr, k);
 
         // for(int i=0;i<a.length;i++){
         //     if(i==0){
@@ -30,11 +35,9 @@ public class date4 {
         //         System.out.println(a[i]);
         //     }
         // }
-
     }
 
-
-    public static int[] problem238(int[] arr){
+    public static int[] problem238(int[] arr) {
         // product of array except self
 
         int n = arr.length;
@@ -42,51 +45,87 @@ public class date4 {
         int[] result = new int[n];
         int[] suffix = new int[n];
         int[] prefix = new int[n];
-        
+
         prefix[0] = 1;
-        suffix[n-1] = 1;
-        
-        for(int i = 1; i<n;i++){
-            prefix[i] = prefix[i-1]*arr[i-1];
+        suffix[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * arr[i - 1];
         }
 
         // [5 , 1 , 4 , 3]
-
         // suffix = [ 12,12,3,1]
-
-        for(int i = n-2; i>=0;i--){
-            suffix[i] = suffix[i+1]*arr[i+1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * arr[i + 1];
         }
 
-        for(int i =0;i<n;i++){
-            result[i] = prefix[i]*suffix[i];
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix[i] * suffix[i];
         }
 
         return result;
 
     }
 
-    public static int problem560(int[] arr, int k){
-        
+    public static int problem560(int[] arr, int k) {
+
         int n = arr.length;
         int c = 0;
+        int ps = 0;
 
-        for(int i = 0;i<n; i++){
-            int currentSum = 0;
-            if(arr[i]==k){
-                c++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); // base case because prefixSum is 0 once.
+
+        for (int i = 0; i < n; i++) {
+            ps += arr[i];
+            if (map.containsKey(ps - k)) {
+                c += map.get(ps - k);
             }
-            for(int j = 0;j<n;j++){
-                currentSum = currentSum + arr[i];
-                if(currentSum == k){
-                    c++;
-                }
-            }            
+
+            map.put(ps, map.getOrDefault(ps, 0) + 1);
+
         }
 
         System.out.println(c);
 
         return c;
+    }
+
+    public static int problem974(int[] arr, int k) {
+
+        // key concept : if remainder of 2 prefix sums are same then it would be divisible by k
+
+        int n = arr.length;
+        int c = 0;
+
+        // int prefix_sum = 0;
+        int[] prefix_sum = new int[n];
+        int[] mod_array = new int[n];
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        prefix_sum[0] = arr[0];
+        
+        map.put(0,1);
+        mod_array[0] = ((prefix_sum[0]%k)+k)%k;
+        for(int i = 1 ; i<n;i++){
+            prefix_sum[i]= prefix_sum[i-1]+arr[i];
+            mod_array[i] = ((prefix_sum[i]%k)+k)%k;
+            // map.put(mod_array[i], 1);
+        }
+
+        for(int j = 0;j<n;j++){
+            if(map.containsKey(mod_array[j])){
+                c+=map.get(mod_array[j]);
+            }
+            map.put(mod_array[j], map.getOrDefault(mod_array[j], 0)+1);
+            
+        }
+        
+        System.out.println(c);
+
+        return 0;
     }
 
 }
