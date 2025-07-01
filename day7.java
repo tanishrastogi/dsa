610fcd610e6cf9d5974609bf65272fecd9913c3f
charlie0054a <charliefernandis28@gmail.com>
2025-07-01 09:47:53 +0530

    problem 26 solved

--- day7.java
+++ day7.java
@@ -87,7 +87,7 @@ public class day7 {
                     left++;
                     right--;
 
-                }
+                 }
                 if(sum<k){
                     left++;
                 }
@@ -98,5 +98,38 @@ public class day7 {
             }
         }
         return res;
+    }
+
+    public static int problem11(){
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int n = height.length;
+        int left = 0, right = n-1;
+        int maxArea = 0;
+        while(left<right){
+            int area = Math.min(height[left], height[right])*(right-left);
+            maxArea = Math.max(maxArea, area);
+            if(height[left]<height[right]) left++;
+            if(height[left]>height[right]) right--;
+        }
+
+        return maxArea;
+    }
+
+    public static int problem26(){
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int n = nums.length;
+        int insertPos = 1;
+        for(int i = 1;i<n;i++){
+            if (nums.length == 0) return 0;    
+
+            if(nums[i] != nums[i-1]){
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }   
+
+
+        }
+
+        return insertPos;
     }
 }
